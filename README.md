7340c2ec1718496a884aa354ac4037f121e24b6f
g.casiano <g.casiano@protonmail.com>
2021-09-03 07:56:57 +1000

    first rocket route and tailwind

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Expirement with crypto layer 1 platform statistics.
 
 ## Layer 1's to compare
+
 - radix
 - eth
 - solana
@@ -11,20 +12,27 @@ Expirement with crypto layer 1 platform statistics.
 - near
 - iota
 
+## links
+
+[layer-1 Frame work for comparison](https://www.theblockcrypto.com/post/114225/layer-1-platforms-a-framework-for-comparison)
+
 ## Front end stack
+
 [Astro](https://astro.build/)
 [Tailwind](https://tailwindcss.com/)
 
 ## Api stack
+
+[Rust](https://www.rust-lang.org/)
 [Rocket](https://rocket.rs/)
 
 ## notes and tutorials
+
 [betterprogramming.com](https://betterprogramming.pub/rest-api-in-rust-step-by-step-guide-b8a6c5fcbff0)
+[blog rocket](https://blog.logrocket.com/rust-web-apps-using-rocket-framework/)
 [https://docs.astro.build/getting-started](https://docs.astro.build/getting-started)
 
-
 ## TODO
-another test
-evern moermer
-more
-asdfas
+
+- [x]
+- [ ]
--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -1,35 +1,13 @@
-use rand::Rng;
-use std::cmp::Ordering;
-use std::io;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/world")]
+fn world() -> &'static str {
+    "Hello, world!"
+}
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    loop {
-        println!("Please we are out here input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    rocket::ignite().mount("/hello", routes![world]).launch();
 }
